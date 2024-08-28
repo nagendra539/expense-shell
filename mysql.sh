7b04e49deaac91bf8accd8a87aa68ecc42b73133
Nagendra Chunduru <nagendra@Nagendras-MacBook-Air.local>
2024-08-28 13:00:34 +0530

    committed

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LOGS_FOLDER="/var/log/expense"
-SCRIPT_NAME=$(echo $0 | cut -d "." -F1 )
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="${LOGS_FOLDER}/${SCRIPT_NAME}-${TIME_STAMP}.log"
 mkdir -p ${LOGS_FOLDER}
@@ -35,7 +35,7 @@ VALIDATE $? "mysql-server"
 systemctl statrt mysqld &>>${LOG_FILE}
 VALIDATE $? "mysql-server"
 
-# This is for idempotency. we can run the sript many times, behavior will not change.
+# This is for idempotency in nature. we can run the sript many times, behavior will not change.
 mysql -h mysql.sn2.online -u root -pExpenseApp@1 -e 'show databases;' &>>${LOG_FILE}
 if [ $? -ne 0 ]
 then
